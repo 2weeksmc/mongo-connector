7de4931650ec4b1c80a0f0288622522e77a01dca
ezTxmMC <eztxmmc@hotmail.com>
2025-04-02 22:39:32 +0200

    added models to mongo connector

--- src/main/java/com/twoweeksmc/connector/MongoConnector.java
+++ src/main/java/com/twoweeksmc/connector/MongoConnector.java
@@ -1,5 +1,7 @@
 package com.twoweeksmc.connector;
 
+import com.twoweeksmc.connector.model.PlayerModel;
+import com.twoweeksmc.connector.model.ServerModel;
 import org.bson.Document;
 
 import com.mongodb.client.MongoClient;
@@ -13,12 +15,24 @@ public class MongoConnector {
     private final MongoClient mongoClient;
     private final MongoCollection<Document> serverCollection;
     private final MongoCollection<Document> playerCollection;
+    private final PlayerModel playerModel;
+    private final ServerModel serverModel;
 
     public MongoConnector(JsonConfig databaseConfiguration) {
         this.mongoClient = MongoClients.create(databaseConfiguration.get("url").asString());
         MongoDatabase mongoDatabase = this.mongoClient.getDatabase(databaseConfiguration.get("database").asString());
         this.serverCollection = mongoDatabase.getCollection("servers");
         this.playerCollection = mongoDatabase.getCollection("players");
+        this.playerModel = new PlayerModel(this);
+        this.serverModel = new ServerModel(this);
+    }
+
+    public ServerModel getServerModel() {
+        return serverModel;
+    }
+
+    public PlayerModel getPlayerModel() {
+        return playerModel;
     }
 
     public MongoCollection<Document> getPlayerCollection() {
